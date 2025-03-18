c661ac35f9cd115ca36703e802c396122730aa6f
jatinjr11 <159682205+jatinjr11@users.noreply.github.com>
2025-03-18 09:10:41 +0000

    array part 2

---
+++ chai aur js/lec-15/Demo1.js
@@ -0,0 +1,41 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+
+
+/*  //Concatinate two arrays
+
+// push existing array pr hi push krta h
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+concat returns new array
+marvel_heros.concat(dc_heros);
+// const allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);
+
+// spread operator
+const all_new_heros = [...marvel_heros, ...dc_heros]; 
+console.log(all_new_heros);
+
+*/
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const real_another_array = another_array.flat(1)
+// const real_another_array = another_array.flat(2)
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+// console.log(another_array);
+
+// console.log(Array.isArray("indore")); // false
+// console.log(Array.from("Indore")); // ['I','n','d', 'o' ,'r','e']
+// console.log(Array.from({name:"ram",age:20})); // intresting
+// console.log(Array.from({name:"10"})); // intresting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+// console.log(Array.of(score1, score2, score3, "Ind", "Canada"));
+// console.log(Array.from(score1)); // error
