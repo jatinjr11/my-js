f212fd01804603e0adfb72f0915681b116c14261
jatinjr11 <159682205+jatinjr11@users.noreply.github.com>
2025-03-19 03:08:12 +0000

    json api

---
+++ chai aur js/objects/Demo3.js
@@ -0,0 +1,28 @@
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+// const { courseInstructor } = course
+// console.log(courseInstructor);
+
+const { courseInstructor: instructor } = course
+// console.log(instructor);
+
+
+// JSOn
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
