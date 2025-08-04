87ea56f174e666203f02ed9df7b91a4024aead4c
Jatin <jatin333x@gmail.com>
2025-08-04 11:38:58 +0530

    undefined vs null

---
+++ my-js-master/js/01_basics/Demo2.js
@@ -0,0 +1,21 @@
+// undefined vs null
+
+/*
+console.log(2 == '2');
+console.log(2 === '2');
+*/
+
+/*
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(null*1);
+console.log(undefined*1);
+*/
+
+/*
+console.log(true == 1);
+console.log(true === 1);
+console.log(true*1);
+console.log(false*1);
+*/
+
