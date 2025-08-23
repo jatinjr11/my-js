c1afe8ab7eff0ad6cab2be32505402164666eaef
Jatin <jatin333x@gmail.com>
2025-08-23 22:46:42 +0530

    default para...

--- my-js-master/js/lec-19  function/Demo4.js
+++ my-js-master/js/lec-19  function/Demo4.js
@@ -1,6 +1,8 @@
+// default parameters : allows you to specify a default value for a function parameter if no value or undefined is passed.
 function greet(name = "Default Name"){
     console.log("Hello,",name )
 }
 
 // greet() // Hello, Default Name
+// greet(undefined) // Hello, Default Name
 greet("Sachin") // Hello, Sachin
