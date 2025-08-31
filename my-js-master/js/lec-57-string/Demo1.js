2afac4ff0cd962c0b1814dae7dcd2f8bd7c8a6fd
Jatin <jatin333x@gmail.com>
2025-08-31 10:23:48 +0530

    string

---
+++ my-js-master/js/lec-57-string/Demo1.js
@@ -0,0 +1,39 @@
+/*
+let s1 = "1st String";
+let s2 = '2nd String';
+let s3 = `3rd 
+String`;
+
+// console.log(s1);
+// console.log(s2);
+// console.log(s3);
+
+// let firstName = new String("Sachin")
+// console.log(firstName);
+*/
+
+let op1 = "English";
+let op2 = "Hindi";
+
+let ans = `${op1} and ${op2}`;
+// console.log(ans);
+
+// length
+// console.log(op1.length)
+
+//substring
+let str = "Babbar";
+// console.log(str.substring(2,6)); // ending index is exclusive
+
+let sentence = "This is my first sentence";
+let words = sentence.split(" ");
+// console.log(words)
+
+let sentence2 = "this is \"my\" 2nd sentence"
+let words2 = sentence.split(" ");
+console.log(words2)
+console.log(words2.join("-"));
+
+
+
+
