75783ce6dd57a1ae4a6987e2ebe38666d4209a90
Jatin <jatin333x@gmail.com>
2025-10-08 09:38:57 +0530

    basics

---
+++ my-js-master/js/01_basics/Demo3.js
@@ -0,0 +1,9 @@
+/*
+console.log(10+"20"); // 1020
+console.log("10"+20); // 1020
+console.log("20" - 10) // 10
+console.log(20 - "10")
+*/
+
+let value1 = null;
+console.log(value1);
--- my-js-master/js/hoisting/Demo1.js
+++ my-js-master/js/hoisting/Demo1.js
@@ -1,5 +1,20 @@
+
+console.log("var",x);
+var x = 10;
+console.log("var",x);
+
+console.log("let",y);
+let y = 20;
+console.log("let",y);
+
+
+
+/*
+
 function demo() {
-    x = 10; // ❌ Without declaration, `x` becomes global!
-  }
-  demo();
-  console.log(x); // 10 (leaked to global scope)
+  x = 10; // ❌ Without declaration, `x` becomes global!
+}
+demo();
+console.log(x); // 10 (leaked to global scope)
+
+*/
