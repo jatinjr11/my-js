5de0e88e5952f0a08c7650f24605e87c43d0483d
Jatin <jatin333x@gmail.com>
2025-10-09 21:07:36 +0530

    currying in js

--- my-js-master/js/lec-19_function/Demo9.js
+++ my-js-master/js/lec-19_function/Demo9.js
@@ -9,6 +9,18 @@ function addTotal(value) {
     return total;
 }
 
-console.log(addTotal(5));
+// console.log(addTotal(5));
 
-console.log(addTotal(5))
+// console.log(addTotal(5))
+
+// what is currying ?
+// it is a technique to convert a function with multiple arguments into a series of functions with a single argument
+// it is a way to create a function that can be called with a single argument and then return a new function that can be called with the next argument
+// Example :-  
+function add(a) {
+    return function (b) {
+        return a + b;
+    }
+}
+
+console.log("Dekho -> ",add(5)(5)); // 10
