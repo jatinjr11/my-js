56dad5fe52f70ee0a3a17b5b4dc9cf80c2adcf92
Jatin <jatin333x@gmail.com>
2025-10-12 21:56:40 +0530

    form validation

---
+++ my-js-master/js/lec-57-string/Demo2.js
@@ -0,0 +1,31 @@
+// concatenate strings
+
+/*
+// 1st way
+let s1 = "Sachin ";
+let s2 = "Kumar";
+console.log(s1+s2);
+
+
+// 2nd way
+let s1 = "Sachin "
+let s2 = "Kumar"
+let s3 = s1.concat(s2);
+console.log(s3);
+
+
+// 3rd way
+let s1 = "Sachin ";
+let s2 = "Kumar";
+let s3 = `${s1} ${s2}`
+console.log(s3);
+
+
+// 4th way
+let s1 = "Sachin ";
+let s2 = "Kumar";
+let s3 = [s1,s2];
+let s4 = s3.join(' ');
+console.log(s3);
+
+*/
