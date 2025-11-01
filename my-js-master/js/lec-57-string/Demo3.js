a68459f52078b7528d4a2672333c82167a503d6b
Jatin <jatin333x@gmail.com>
2025-11-01 17:22:08 +0530

    string changes

---
+++ my-js-master/js/lec-57-string/Demo3.js
@@ -0,0 +1,10 @@
+/*
+let str1 = "Sachin";
+// console.log(str1.substring(2));
+console.log(str1.substring(2,4)); // ending index is exclusive
+*/
+
+// split method
+let sentence = "This is my first sentence";
+let words = sentence.split(" ");
+console.log(words);
