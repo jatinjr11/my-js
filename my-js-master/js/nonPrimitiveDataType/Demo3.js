65dde8183cddbd0db62edb9dc3eaf8b7971dc27a
Jatin <jatin333x@gmail.com>
2026-01-12 17:09:52 +0530

    non primi d.t

---
+++ my-js-master/js/nonPrimitiveDataType/Demo3.js
@@ -0,0 +1,14 @@
+let a = 10;
+a = 60;
+console.log("a: ", a);
+
+
+/*
+| Feature    | Primitive | Non-Primitive |
+| ---------- | --------- | ------------- |
+| Storage    | By value  | By reference  |
+| Mutability | Immutable | Mutable       |
+| Memory     | Stack     | Heap          |
+| Size       | Fixed     | Dynamic       |
+| Comparison | By value  | By reference  |
+*/
