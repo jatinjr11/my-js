fc2117646b39190e3ae041dbb041987039365b68
Jatin <jatin333x@gmail.com>
2026-01-12 15:58:18 +0530

    primi d.t

--- my-js-master/js/primitiveDataType/Demo1.js
+++ my-js-master/js/primitiveDataType/Demo1.js
@@ -1,3 +1,22 @@
+
+/*
+
+-> Primitive data types store a single simple value
+-> They are immutable (cannot be changed directly)
+
+Type	         Example
+Number	  ->     10, 3.14
+String	  ->     "hello"
+Boolean	  ->     true, false
+Undefined ->      x;
+Null	  ->     let y = null;
+BigInt	  ->     123n
+Symbol	  ->     Symbol("id")
+
+
+
+*/
+
 let str = "hello";
-str[0] = "H"; // ❌ Fails silently (strings are immutable)
+str[0] = "K"; // ❌ Fails silently (strings are immutable)
 console.log(str); // "hello" (unchanged)
