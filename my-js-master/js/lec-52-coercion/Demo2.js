0a67c229a82e919e332944e2be731c5a12a0a1d3
Jatin <jatin333x@gmail.com>
2026-01-14 11:13:06 +0530

    type casting

---
+++ my-js-master/js/lec-52-coercion/Demo2.js
@@ -0,0 +1,11 @@
+/*  Explicit type casting
+Number("10") // 10
+String(100) // "100"
+Boolean(0)   // false
+*/
+
+let age = "18";
+let myAge = Number(age);
+
+console.log(typeof age);
+console.log(typeof myAge);
