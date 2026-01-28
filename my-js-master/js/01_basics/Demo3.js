18edf6c76611035ee8f5571dcd0d7076f9c4019b
Jatin <jatin333x@gmail.com>
2026-01-28 15:54:04 +0530

    basic changes

--- my-js-master/js/01_basics/Demo3.js
+++ my-js-master/js/01_basics/Demo3.js
@@ -1,4 +1,3 @@
-/*
 console.log(10+"20"); // 1020
 console.log("10"+20); // 1020
 console.log("20" - 10) // 10
@@ -6,11 +5,12 @@ console.log(20 - "10")
 
 let value1 = null;
 console.log(value1);
-*/
 
+/*
 function demo() {
     console.log(arguments[0]);
     console.log(arguments[1]);
 }
 
 demo(10, 20);
+*/
