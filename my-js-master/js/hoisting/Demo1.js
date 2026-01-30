b3d2b0c03283cc8a0ada8fa043ab0bc1b49790f6
Jatin <jatin333x@gmail.com>
2026-01-30 16:53:17 +0530

    hoisting update

--- my-js-master/js/hoisting/Demo1.js
+++ my-js-master/js/hoisting/Demo1.js
@@ -3,13 +3,13 @@ console.log("var",x);
 var x = 10;
 console.log("var",x);
 
+/*
 console.log("let",y);
 let y = 20;
 console.log("let",y);
 
 
 
-/*
 
 function demo() {
   x = 10; // ❌ Without declaration, `x` becomes global!
